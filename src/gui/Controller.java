2b9fbae1fc46536d0492922ba3259754848e97e8
Riley Johnson <rjohnson01@email.wm.edu>
2021-12-28 14:42:08 -0500

    Base start code for the game

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -2,4 +2,32 @@ package gui;
 
 public class Controller {
     
+    // States for the game
+    GameState[] states;
+
+    // Current state of the game
+    GameState currentState;
+
+    // Panel used to draw on the screen
+    MapPanel panel;
+
+    Controller() {
+        states = new GameState[5];
+        states[0] = new StateTitle();
+        states[1] = new StateGeneration();
+        states[2] = new StatePlaying();
+        states[3] = new StateWinning();
+        states[4] = new StateLoosing();
+        currentState = states[0];
+        panel = new MapPanel();
+    }
+
+    public MapPanel getPanel() {
+        return panel;
+    }
+
+    public void start() {
+        currentState.start(this, panel);
+    }
+
 }
--- src/gui/GetOutGame.java
+++ src/gui/GetOutGame.java
@@ -1,9 +1,23 @@
 package gui;
 
-public class GetOutGame {
+import javax.swing.JFrame;
+import java.awt.event.KeyListener;
+
+public class GetOutGame extends JFrame{
     
     GetOutGame() {
-        
+        Controller controller = new Controller();
+        add(controller.getPanel());
+
+        KeyListener keyListener = new InputListener();
+        addKeyListener(keyListener);
+
+        setSize(Constants.VIEW_WIDTH, Constants.VIEW_HEIGHT);
+        setVisible(true);
+
+        setFocusable(true);
+
+        controller.start();
     }
 
 }
