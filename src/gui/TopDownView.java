31defbc8ca90a0b9d218f0f152352c7ab47a60bd
Riley Johnson <rjohnson01@email.wm.edu>
2022-01-03 11:33:43 -0500

    beginnings of topDownView

--- src/gui/TopDownView.java
+++ src/gui/TopDownView.java
@@ -1,5 +1,26 @@
 package gui;
 
+import java.awt.Color;
+
+import generation.Map;
+
 public class TopDownView {
+
+    // colors used for the view
+    static final Color wallColor = Color.gray;
+    static final Color doorColor = Color.black;
+    static final Color enemyShade = Color.red;
+
+    private Map map;
+
+    private boolean zoomed;
+
+
+    public TopDownView(Map map) {
+        this.map = map;
+        zoomed = false;
+    }
+
+    public void draw() {}
     
 }
